7b7a5fbd5d692e55b0452f71d20302c8335cb367
Tyler Morales <moratyle@gmail.com>
2020-01-04 14:46:45 -0600

    fix broken parts

--- app.js
+++ app.js
@@ -2,90 +2,14 @@
 const textEl = document.querySelector('h1')
 const textArr = textEl.textContent.split('')
 
-
 let char = 0
 const userInput = document.querySelector('#user-input')
 
 textEl.textContent = ''
 
-// wrap each letter in output in a span 
-function createInduvidualLetterSpans(letterArr) {
-    for (const letter of letterArr) {
-        const letterEl = document.createElement('span')
-        letterEl.textContent = letter
-        textEl.appendChild(letterEl)
-    }
-}
-
-// animate each letter
-function displayChars() {
-    const span = textEl.querySelectorAll('span')[char]
-    span.classList.add('load')
-    char++
-
-    if (char == textArr.length) {
-        complete()
-        return
-    }
-}
-
-function complete() {
-    clearInterval(timer)
-    timer = null
-}
-
-// iterator function to animate each letter
-function displayChars(arr) {
-    let count = 0
-    const span = textEl.querySelectorAll('span')[char]
-    const inner = {
-        next: function () {
-            const el = arr[count]
-            console.log(span)
-            // span.classList.add('load')
-            count++
-            arr[count] == undefined ? done = true : done = false
-            return {
-                value: el,
-                done
-            }
-        }
-    }
-    return inner
-}
-
-const iterator = displayChars(textArr);
-
-console.log(iterator.next())
-
-
-// let result = iterator.next();
-// while (!result.done) {
-//     result = iterator.next()
-//     console.log(result)
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// display story game
 createInduvidualLetterSpans(textArr)
-//let timer = setInterval(displayChars, 10)
-
-
-
-
-
-
+let timer = setInterval(displayChars, 10)
 
 // change computer color
 userInput.addEventListener('submit', (e) => {
@@ -94,3 +18,5 @@ userInput.addEventListener('submit', (e) => {
 
     changeTerminalTextColor(input)
 })
+
+
--- functions.js
+++ functions.js
@@ -1,3 +1,28 @@
+// wrap each letter in output in a span 
+function createInduvidualLetterSpans(letterArr) {
+    for (const letter of letterArr) {
+        const letterEl = document.createElement('span')
+        letterEl.textContent = letter
+        textEl.appendChild(letterEl)
+    }
+}
+
+// animate each letter
+function displayChars() {
+    const span = textEl.querySelectorAll('span')[char]
+    span.classList.add('load')
+    char++
+
+    if (char == textArr.length) {
+        complete()
+        return
+    }
+}
+
+function complete() {
+    clearInterval(timer)
+    timer = null
+}
 // change terminal color based on user input
 function changeTerminalTextColor(computer) {
     switch (computer) {
