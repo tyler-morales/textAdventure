5266b05da2bf76744a31d2da25659e2654f3713c
Tyler Morales <moratyle@gmail.com>
2020-01-04 09:26:05 -0600

    add styles

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const textEl = document.querySelector('h1')
 const str = textEl.textContent
 const lettersArr = str.split('')
+const userInput = document.querySelector('#user-input')
+
 textEl.textContent = ''
 
 for (const letter of lettersArr) {
@@ -21,7 +23,38 @@ function onTick() {
     }
 }
 
+
+
+userInput.addEventListener('submit', (e) => {
+    let text = e.target.elements.text.value.toLowerCase()
+    e.preventDefault()
+
+    if (text == 'ibm') {
+        changeTerminalTextColor(ibm = 'ibm')
+    }
+    if (text == 'cathode') {
+        changeTerminalTextColor(cathode = 'cathode')
+    }
+    if (text == 'apple') {
+        changeTerminalTextColor(appleii = 'apple')
+    }
+})
+
 function complete() {
     clearInterval(timer)
     timer = null
+}
+
+const changeTerminalTextColor = (color) => {
+    if (color == 'ibm') {
+        document.documentElement.setAttribute('data-theme', 'ibm')
+
+
+    }
+    if (color == 'cathode') {
+        document.documentElement.setAttribute('data-theme', 'cathode')
+    }
+    if (color == 'apple') {
+        document.documentElement.setAttribute('data-theme', 'apple')
+    }
 }
